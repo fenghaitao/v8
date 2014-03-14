164ee55c40d85e62d51687e66210c9b63a4c04a1
Haitao Feng <haitao.feng@intel.com>
2014-03-14 12:59:21 +0800

    Rebased with 4880ed9529003ce9498e5cbbc6792d9cd5da9e5d

--- src/x32/macro-assembler-x32.h
+++ src/x32/macro-assembler-x32.h
@@ -1369,6 +1369,10 @@ class MacroAssembler: public Assembler {
                                   Register filler);
 
 
+  // Emit code for a flooring division by a constant. The dividend register is
+  // unchanged, the result is in rdx, and rax gets clobbered.
+  void FlooringDiv(Register dividend, int32_t divisor);
+
   // ---------------------------------------------------------------------------
   // StatsCounter support
 
